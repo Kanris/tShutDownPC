e8380d58311ec82669e8ebe058957685097cdf57
kanris2 <kanris2@gmail.com>
2020-01-28 20:30:09 +0200

    Add project files.

---
+++ tShutDownPC/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace tShutDownPC
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
